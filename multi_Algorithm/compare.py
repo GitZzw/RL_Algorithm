2563651bad2e586b58180c890fdb0f079a91a620
GitZzw <120137016@qq.com>
2020-12-12 21:13:14 +0800

    uload

--- multi_Algorithm/compare.py
+++ multi_Algorithm/compare.py
@@ -366,23 +366,44 @@ def compareMethods():
     for agent_i in range(2): benchmark_data[agent_i][0] = agents[agent_i].benchmark(eps_benchmark)
     # Train and benchmark agents
     for point_i in range(1, n_plot_points):
-        for agent_i in range(2):
-            print('Agent ' + str(agent_i) + ', Episode ' + str((point_i+1)*eps_per_point))
-            agents[agent_i].train(eps_per_point)
-            benchmark_data[agent_i][point_i] = agents[agent_i].benchmark(eps_benchmark)
+        # for agent_i in range(2):
+        #     print('Dyna ' + str(agent_i) + ', Episode ' + str((point_i+1)*eps_per_point))
+        #     agents[agent_i].train(eps_per_point)
+        #     benchmark_data[agent_i][point_i] = agents[agent_i].benchmark(eps_benchmark)
+
+        print('DynaQ' + ', Episode ' + str((point_i+1)*eps_per_point))
+        agents[0].train(eps_per_point)
+        benchmark_data[0][point_i] = agents[0].benchmark(eps_benchmark)
+        print('Dyna2' + ', Episode ' + str((point_i+1)*eps_per_point))
+        agents[1].train(eps_per_point)
+        benchmark_data[1][point_i] = agents[1].benchmark(eps_benchmark)
+
 
     # Plot results
-    plt.figure(figsize=(16, 10))
-    xaxis = [eps_per_point*(i+1) for i in range(n_plot_points)]
-    title1 = 'DynaQ, n = ' + str(plan1) + ', a = ' + str(alpha1)
-    title4 = 'Dyna2, n = ' + str(plan4) + ', a = ' + str(alpha4) + ', b = ' + str(beta4)
-    titles = [title1, title4]
-    for i in range(2):
-        plt.subplot(221+i)
-        plt.plot(xaxis, benchmark_data[i])
-        plt.xlabel('Training episodes')
-        plt.ylabel('Average reward per episode')
-        plt.title(titles[i])
-    plt.show()
+    # plt.figure(figsize=(16, 10))
+    # xaxis = [eps_per_point*(i+1) for i in range(n_plot_points)]
+    # title1 = 'DynaQ'
+    # title4 = 'Dyna2'
+    # titles = [title1, title4]
+    # for i in range(2):
+    #     plt.subplot(221+i)
+    #     plt.plot(xaxis, benchmark_data[i])
+    #     plt.xlabel('Training episodes')
+    #     plt.ylabel('Average reward per episode')
+    #     plt.title(titles[i])
+    # plt.show()
+        # Plot results
+        plt.figure(figsize=(16, 10))
+        xaxis = [eps_per_point*(i+1) for i in range(n_plot_points)]
+        title1 = 'DynaQ'
+        title4 = 'Dyna2'
+        titles = [title1, title4]
+        for i in range(2):
+            plt.subplot(221+i)
+            plt.plot(xaxis, benchmark_data[i])
+            plt.xlabel('Training episodes')
+            plt.ylabel('Average reward per episode')
+            plt.title(titles[i])
+        plt.show()
 
 compareMethods()
